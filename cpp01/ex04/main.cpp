acf92920a9ea900153bc4a76f4127e253d467e00
jeekpark <jeekpark@student.42seoul.kr>
2023-10-02 15:29:49 +0900

    cpp01-ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 15:03:38 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/10/01 15:50:55 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/10/02 15:28:36 by jeekpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,48 @@
 #include <fstream>
 #include <sstream>
 
-std::string replaceString(std::string subject, const std::string& search, const std::string& replace) {
-    size_t pos = 0;
-    while ((pos = subject.find(search, pos)) != std::string::npos) {
-        subject.replace(pos, search.length(), replace);
-        pos += replace.length();
-    }
-    return subject;
+std::string replaceString( std::string content, const std::string& search, const std::string& replace )
+{
+	size_t	pos = 0;
+	while ( true )
+	{
+		pos = content.find( search, pos );
+		if ( pos == std::string::npos )
+			break;
+		content = content.substr( 0, pos ) + replace + content.substr( pos + search.length() );
+		pos += replace.length();
+	}
+	return content;
 }
 
-int	main ( int argc, char **argv )
+int	main( int argc, char **argv )
 {
-	if ( argc != 4)
+	if ( argc != 4 )
 	{
 		std::cerr << "Usage: ./replace <filename> <s1> <s2>." << std::endl;
-		return 1; 
+		return EXIT_FAILURE; 
 	}
-	std::ifstream	file(argv[1]);
+	if ( std::string(argv[2]).empty() == true )
+	{
+		std::cerr << "Error: <s1> shouldn't be empty string." << std::endl;
+		return EXIT_FAILURE;
+	}
+	std::ifstream	file( argv[1], std::ios::in | std::ios::out );
 	if ( file.is_open() == false )
 	{
 		std::cerr << "Error: Unable to open file." << std::endl;
-		return 1;
+		return EXIT_FAILURE;
 	}
-
-	std::ostringstream	ss;
-	ss << file.rdbuf();
-	std::string content = ss.str();
-
-	content = replaceString(content, argv[2], argv[3]);
-
+	std::ostringstream	stringStream;
+	stringStream << file.rdbuf();
 	file.close();
-	std::ofstream	outFile("heloo");
+	std::string content = stringStream.str();
+
+	content = replaceString( content, argv[2], argv[3] );
+
+	std::ofstream	outFile( std::string( argv[1] ) + ".replace" );
 	outFile << content;
 	outFile.close();
-	return (0);
+
+	return EXIT_SUCCESS;
 }
