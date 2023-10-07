1fc3c093090f1e898940bd80c1a4e75d6a282212
jeekpark <jeekpark@student.42seoul.kr>
2023-10-07 17:29:31 +0900

    cpp02-ex03

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 23:51:11 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/10/07 17:10:58 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/10/07 17:29:08 by jeekpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,21 @@
 
 Fixed	crossProduct( const Point& a, const Point& b, const Point& c )
 {
-	return ( b.getY() - c.getY() ) * ( a.getX() - c.getX() )
-			+ ( c.getX() - b.getX() ) * ( a.getY() - c.getY() );
+	return ( b.getY() - c.getY() ) * ( a.getX() - c.getX() ) + ( c.getX() - b.getX() ) * ( a.getY() - c.getY() );
 }
-
-/* bool	bsp( const Point a, const Point b, const Point c, const Point point)
-{
-	Fixed	totalArea( ( b.getY() - c.getY() ) * ( a.getX() - c.getX() )
-							+ ( c.getX() - b.getX() ) * ( a.getY() - c.getY() ) );
-	if ( totalArea == 0.0f) return false;
-	Fixed	aW = ( ( b.getY() - c.getY() ) * ( point.getX() - c.getX() ) 
-					+ (c.getX() - b.getX() ) * ( point.getY() - c.getY() ) )
-					/ totalArea;
-	Fixed	bW = ( ( c.getY() - a.getY() ) * ( point.getX() - c.getX() ) 
-					+ (a.getX() - c.getX() ) * ( point.getY() - c.getY() ) )
-					/ totalArea;
-	Fixed	cW = Fixed( 1.0f ) - aW - bW;
-
-	if ( aW >= 0.0f && aW <= 1.0f
-		&& bW >= 0.0f && bW <= 1.0f
-		&& cW >= 0.0f && cW <= 1.0f )
-		return true;
-	else return false;
-} */
 
 bool	bsp( const Point a, const Point b, const Point c, const Point point)
 {
 	Fixed	totalArea( crossProduct( a, b, c ) );
-	if ( totalArea == 0.0f )
-		return false;
 	Fixed	aW = crossProduct( point, b, c ) / totalArea;
 	Fixed	bW = crossProduct( point, c, a ) / totalArea;
 	Fixed	cW = Fixed( 1.0f ) - aW - bW;
-
-	if ( aW >= 0.0f && aW <= 1.0f
-		&& bW >= 0.0f && bW <= 1.0f
-		&& cW >= 0.0f && cW <= 1.0f )
+	
+	if ( totalArea == 0.0f )
+		return false;
+	else if ( aW > 0.0f && aW < 1.0f
+		&& bW > 0.0f && bW < 1.0f
+		&& cW > 0.0f && cW < 1.0f ) 
 		return true;
 	else
 		return false;
--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 20:25:03 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/10/07 17:11:42 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/10/07 17:28:35 by jeekpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 int main( void )
 {
-	if ( bsp( Point( 0.0f, 0.0f ), Point( 0.0f, 0.0f ),
-					Point( 0.0f, 10.0f ), Point( 0.0f, 0.0f ) ) == true)
+	if ( bsp( Point( 0.0f, 0.0f ), Point( 10.0f, 0.0f ),
+					Point( 0.0f, 10.0f ), Point( 2.0f, 2.0f ) ) == true)
 		std::cout << "true" << std::endl;
 	else
 		std::cout << "fasle" << std::endl;
