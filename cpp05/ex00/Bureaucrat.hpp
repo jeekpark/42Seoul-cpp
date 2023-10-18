5f0370b6c45bb09add280b890d528915f21ef451
jeekpark <jeekpark@student.42seoul.kr>
2023-10-18 18:46:44 +0900

    cpp05

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 18:22:23 by jeekpark          #+#    #+#             */
+/*   Updated: 2023/10/18 18:46:17 by jeekpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+
+class Bureaucrat
+{
+	private:
+		const std::string	mName;
+		int8_t				mGrade;
+	
+
+};
+
+#endif
