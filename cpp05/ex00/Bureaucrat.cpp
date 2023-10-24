b6177b8f9decf71a31a89662a8ce8cecad272e0f
jeekpark <jeekpark@student.42seoul.kr>
2023-10-25 00:22:32 +0900

    cpp05

---
+++ cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 22:01:40 by jeekpark          #+#    #+#             */
+/*   Updated: 2023/10/25 00:17:20 by jeekpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+
+Bureaucrat::Bureaucrat(std::string name, uint8_t grade)
+: mName(name)
+{
+	setGrade(grade);
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& copy)
+: mName(copy.getName())
+{
+	setGrade(copy.getGrade());
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+	// Empty function body
+}
+
+
+const std::string&	Bureaucrat::getName(void) const
+{
+	return mName;
+}
+
+uint8_t	Bureaucrat::getGrade(void) const
+{
+	return mGrade;
+}
+
+
+void	Bureaucrat::increaseGrade(void)
+{
+	setGrade(getGrade() - 1);
+}
+
+void	Bureaucrat::decreaseGrade(void)
+{
+	setGrade(getGrade() + 1);
+}
+
+
+
+
+
+
+void	Bureaucrat::setGrade(uint8_t grade)
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	mGrade = grade;
+}
+
+
+
+Bureaucrat::Bureaucrat(void)
+: mName("<null>")
+{
+	setGrade(150);
+}
+
+Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& copy)
+{
+	return *this;
+}
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,22 +6,47 @@
 /*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 18:22:23 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/10/18 18:46:17 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/10/25 00:17:21 by jeekpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #ifndef BUREAUCRAT_HPP
-# define BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
 
-# include <string>
+#include <string>
+#include <exception>
+#include <iostream>
+
+#define unsigned char uint8_t;
 
 class Bureaucrat
 {
-	private:
-		const std::string	mName;
-		int8_t				mGrade;
+public:
+						Bureaucrat(std::string name, uint8_t grade);
+						Bureaucrat(const Bureaucrat& copy);
+						~Bureaucrat(void);
+
+	const std::string&	getName(void) const;
+	uint8_t				getGrade(void) const;
+
+	void				increaseGrade(void);
+	void				decreaseGrade(void);
+
+	class GradeTooHighException : public std::exception {};
+	class GradeTooLowException : public std::exception {};
+	class InvalidConstructorException : public std::exception {};
+	class InvalidOperatorException : public std::exception {};
+private:
 	
+	const std::string	mName;
+	uint8_t				mGrade;
+
+						Bureaucrat(void);
+	Bureaucrat&			operator=(const Bureaucrat& copy);
+	
+	void				setGrade(uint8_t grade);
+
 
 };
 
