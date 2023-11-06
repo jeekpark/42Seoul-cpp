3a0c78817b4373b017734d8486ca886d4b9a3856
jeekpark <jeekpark@student.42seoul.kr>
2023-11-06 15:41:34 +0900

    cpp06-ex02

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/27 23:57:43 by jeekpark          #+#    #+#             */
+/*   Updated: 2023/11/06 15:41:06 by jeekpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctime>
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+
+Base*	generate(void)
+{
+	
+	switch (std::rand() % 3)
+	{
+	case 0:
+		return new A();
+	case 1:
+		return new B();
+	case 2:
+		return new C();
+	}
+	return (Base *)0;
+}
+
+void	identify(Base* p)
+{
+	if (dynamic_cast<A *>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Unable to identify." << std::endl;
+}
+
+void	identify(Base& p)
+{
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+		return ;
+	}
+	catch (const std::exception&)
+	{}
+
+	try
+	{
+		(void)dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+		return ;
+	}
+	catch (const std::exception&)
+	{}
+
+	try
+	{
+		(void)dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+		return ;
+	}
+	catch (const std::exception&)
+	{}
+}
+
+int main(int argc, char **argv)
+{
+	std::srand(static_cast<unsigned int>(time(NULL)));
+	for (size_t i = 0; i < 20; ++i)
+	{
+		
+		Base* ptr = generate();
+		identify(ptr);
+		identify(*ptr);
+		delete ptr;
+		ptr = NULL;
+	}
+	return 0;
+}
