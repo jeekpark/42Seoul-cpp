ace617bb1b0fd05a9275560163ff5827290bbd7a
Jeekun Park <jeekpark@cx1r5s3.42seoul.kr>
2023-11-07 16:32:48 +0900

    cpp07

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jeekpark <jeekpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:09:13 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/11/06 16:56:54 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/11/07 15:38:14 by jeekpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,10 @@ template<typename T> void swap(T& a, T& b)
 	b = temp;
 }
 
-template<typename T> T& min(T& a, T& b)
-{
-	if (a < b)
-		return a;
-	else
-	 	return b;
-}
-
-template<typename T> T& max(T& a, T& b)
-{
-	if (a > b)
-		return a;
-	else
-		return b;
-}
+template<typename T> T& min(T& a, T& b) { return a < b ? a : b; }
+template<typename T> T& max(T& a, T& b) { return a > b ? a : b; }
+template<typename T> const T& min(const T& a, const T& b) { return a < b ? a : b; }
+template<typename T> const T& max(const T& a, const T& b) { return a > b ? a : b; }
 
 
 #endif
