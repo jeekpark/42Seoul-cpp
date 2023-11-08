1e7d4b805b77f0b2e62eb8d05600fdf5b04e0321
Jeekun Park <jeekpark@cx1r5s3.42seoul.kr>
2023-11-08 17:08:16 +0900

    cpp07

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,25 +6,24 @@
 /*   By: jeekpark <jeekpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:09:13 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/11/07 15:38:14 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/11/08 17:08:01 by jeekpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#ifndef WHATEVER_H
-#define WHATEVER_H
 
-template<typename T> void swap(T& a, T& b)
-{
-	T temp = a;
-	a = b;
-	b = temp;
-}
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
 
+template<typename T> void swap(T& a, T& b) { T temp = a; a = b; b = temp; }
 template<typename T> T& min(T& a, T& b) { return a < b ? a : b; }
 template<typename T> T& max(T& a, T& b) { return a > b ? a : b; }
 template<typename T> const T& min(const T& a, const T& b) { return a < b ? a : b; }
 template<typename T> const T& max(const T& a, const T& b) { return a > b ? a : b; }
+template<typename T> volatile T& min(volatile T& a, volatile T& b) { return a < b ? a : b; }
+template<typename T> volatile T& max(volatile T& a, volatile T& b) { return a > b ? a : b; }
+template<typename T> volatile const T& min(volatile const T& a, volatile const T& b) { return a < b ? a : b; }
+template<typename T> volatile const T& max(volatile const T& a, volatile const T& b) { return a > b ? a : b; }
 
 
 #endif
