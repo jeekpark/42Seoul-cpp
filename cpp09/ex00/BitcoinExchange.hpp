64419d4f14f937f5e3b6ff694e7c0d8c98392141
jeekpark <jeekpark@student.42seoul.kr>
2023-11-11 20:59:00 +0900

    cpp09

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 20:44:27 by jeekpark          #+#    #+#             */
+/*   Updated: 2023/11/11 20:57:28 by jeekpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+
+
+class BitcoinExchange
+{
+public:
+private:
+};
+
+#endif
