aab010f5d5fc2c0b2df1f9e78ee713eb0dcb60a4
jeekpark <jeekpark@student.42seoul.kr>
2023-11-11 21:05:17 +0900

    cpp09

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,13 +6,22 @@
 /*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:17:06 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/11/11 20:57:02 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/11/11 21:05:00 by jeekpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-int main(void)
+#include <cstdlib>
+#include <iostream>
+
+int main(const int argc, const char** argv)
 {
-		
+	if (argc != 2)
+	{
+		std::cout << "Error: could not open file." << std::endl;
+		return EXIT_FAILURE;
+	}
+	(void)argv;
+
 	return 0;
 }
