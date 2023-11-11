bc741045acb48848acfeaf4dfbae694465acd74b
jeekpark <jeekpark@student.42seoul.kr>
2023-11-11 20:41:35 +0900

    cpp08-ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 00:50:44 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/11/11 19:50:51 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/11/11 20:41:16 by jeekpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,18 @@
 #include <deque>
 #include <iterator>
 
+/**
+ * @brief A stack class template that extends std::stack to provide itrator
+ * 
+ * @tparam Type The type of elements in the stack
+ * @tparam Container The Type of the underlying container to use, defaults to std::deque<Type>
+ */
 template <typename Type, typename Container = std::deque<Type> >
 class MutantStack : public std::stack<Type, Container>
 {
 public:
 	typedef typename Container::iterator iterator;
-	typedef typename Container::const_iterator const_iterator;
-	
+	typedef typename Container::const_iterator const_iterator;	
 	iterator begin(void) { return this->c.begin(); }
 	iterator end(void) { return this->c.end(); }
 	const_iterator begin(void) const { return this->c.begin(); }
