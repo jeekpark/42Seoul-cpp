adafeec898430449dcba584d98c29430147cd9a4
jeekpark <jeekpark@student.42seoul.kr>
2023-11-17 01:43:06 +0900

    cpp09-ex01

---
+++ cpp09/ex01/RPN.cpp
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 20:49:36 by jeekpark          #+#    #+#             */
+/*   Updated: 2023/11/16 23:52:16 by jeekpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RPN.hpp"
+#include <cctype>
+#include <stack>
+
+/* public head */
+RPN::RPN(void) {}
+
+RPN::RPN(const RPN& copy) : mStack(copy.mStack) {}
+
+RPN::~RPN(void) {}
+
+RPN& RPN::operator=(const RPN& copy)
+{
+	if (this != &copy) mStack = copy.mStack;
+	return *this;
+}
+
+void RPN::clear(void)
+{
+	mStack = std::stack<double>();
+}
+
+void RPN::insert(char c)
+{
+	if (std::isdigit(static_cast<int>(c)))
+	{
+		push(c - '0');
+		return;
+	}
+	if (size() < 2)
+		throw RPN::IncompleteExpressionException();
+	double a, b;
+	switch (c)
+	{
+	case '+':
+		a = top(); pop();
+		b = top(); pop();
+		push(b + a);
+		break;
+	case '-':
+		a = top(); pop();
+		b = top(); pop();
+		push(b - a);
+		break;
+	case '*':
+		a = top(); pop();
+		b = top(); pop();
+		push(b * a);
+		break;
+	case '/':
+		a = top(); pop();
+		b = top(); pop();
+		push(b / a);
+		break;
+	default:
+		throw RPN::IncompleteExpressionException();
+	}
+}
+
+double RPN::result(void) const
+{
+	if (size() == 1) return top();
+	else throw RPN::IncompleteExpressionException();
+}
+
+const char* RPN::IncompleteExpressionException::what(void) const throw()
+{
+	return "Incomplete Expression";
+}
+/* public tail */
+
+
+
+
+/* private head */
+void RPN::push(double n)
+{
+	mStack.push(n);
+}
+
+void RPN::pop(void)
+{
+	mStack.pop();
+}
+
+double RPN::top(void) const
+{
+	return mStack.top();
+}
+
+size_t RPN::size(void) const
+{
+	return mStack.size();
+}
+/* private tail */
---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 19:23:49 by jeekpark          #+#    #+#             */
+/*   Updated: 2023/11/16 23:26:51 by jeekpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <exception>
+#include <stack>
+#include <string>
+
+
+class RPN
+{
+public:
+						RPN(void);
+						RPN(const RPN& copy);
+						~RPN(void);
+	RPN&				operator=(const RPN& copy);
+
+	void				clear(void);
+	void				insert(char c);
+	double				result(void) const;
+
+	class IncompleteExpressionException : public std::exception
+	{ public: virtual const char* what(void) const throw(); };
+private:
+	std::stack<double>	mStack;
+
+	void				push(double n);
+	void				pop(void);
+	double				top(void) const;
+	size_t				size(void) const;
+};
+
+#endif
