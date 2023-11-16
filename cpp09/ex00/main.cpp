56082e18c2189e4cd1e596935bcbdcf556fc1efe
jeekpark <jeekpark@student.42seoul.kr>
2023-11-16 19:14:38 +0900

    cpp09-ex00

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,15 +6,12 @@
 /*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:17:06 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/11/16 18:42:48 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/11/16 19:14:04 by jeekpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cstdlib>
-#include <exception>
+
 #include <iostream>
-#include <string>
-#include <vector>
 
 #include "./BitcoinExchange.hpp"
 
@@ -23,7 +20,7 @@ int main(const int argc, const char** argv)
 	if (argc != 2)
 	{
 		std::cout << "Error: could not open file." << std::endl;
-		return EXIT_FAILURE;
+		return 1;
 	}
 	BitcoinExchange exchanger;
 	try
@@ -33,10 +30,11 @@ int main(const int argc, const char** argv)
 		for (std::vector<std::string>::const_iterator it = exchanger.getExchangeResult().begin();
 			it != exchanger.getExchangeResult().end(); ++it)
 			std::cout << *it << '\n';
-	} catch (const std::exception& e)
+	}
+	catch (const std::exception& e)
 	{
 		std::cout << "Error: " << e.what() << std::endl;
-		return EXIT_FAILURE;
+		return 1;
 	}
-	return EXIT_SUCCESS;
+	return 0;
 }
