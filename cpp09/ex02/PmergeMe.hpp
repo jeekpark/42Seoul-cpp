ad7e925b78920319738b1b6174520c55201805db
jeekpark <jeekpark@student.42seoul.kr>
2023-11-17 02:09:01 +0900

    cpp09-ex02

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 01:44:14 by jeekpark          #+#    #+#             */
+/*   Updated: 2023/11/17 02:08:50 by jeekpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+
+class PmergeMe
+{
+public:
+	
+private:
+};
+
+#endif
