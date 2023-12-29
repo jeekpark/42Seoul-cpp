b919fa05361184b16aeaa6898c19143f5fcd9c7a
Jeekun Park <jeekpark@cx1r5s7.42seoul.kr>
2023-12-30 08:09:51 +0900

    feat: PmergeMe.hpp

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeekpark <jeekpark@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jeekpark <jeekpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 01:44:14 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/11/17 15:30:14 by jeekpark         ###   ########.fr       */
+/*   Updated: 2023/12/30 08:09:23 by jeekpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,34 @@
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
+#include <deque>
+#include <list>
+#include <unordered_map>
 
 class PmergeMe
 {
-public:
+public: // OCF
+	PmergeMe();
+	PmergeMe(const PmergeMe& copy);
+	~PmergeMe();
+	PmergeMe& operator=(const PmergeMe& copy);
+
+public: // basic function
+	void pushElement(int num);
+	void sortDeque();
+	void sortList();
 	
+public: // getter function
+	const std::deque<int>& getDeque() const;
+	const std::list<int>& getList() const;
+	const float& getDequeTimeTaken() const;
+	const float& getListTimeTaken() const;
+
 private:
+	std::deque<int>	mDeque;
+	std::list<int>	mlist;
+	float						mDequeTimeTaken;
+	float						mListTimeTaken;
 };
 
 #endif
