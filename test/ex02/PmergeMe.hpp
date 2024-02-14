22f81265a9d21dfb23f9dded16e28ccda63aed53
JEEKUN PARK <118240517+jeekpark@users.noreply.github.com>
2024-02-14 03:28:27 +0000

    feat: cpp09

--- test/ex02/PmergeMe.hpp
+++ test/ex02/PmergeMe.hpp
@@ -11,6 +11,8 @@
 #include <algorithm>
 #include <limits>
 
+#include "unordered_map.hpp"
+
 class PmergeMe
 {
 public:
@@ -36,50 +38,6 @@ private:
   const char** mArgv;
   std::deque<size_t> mSequenceDeque;
   std::vector<size_t> mSequenceVector; 
-private:
-  template<typename Tkey, typename Tvalue, typename Tcontainer = std::vector<std::pair<Tkey, Tvalue > > >
-  class unordered_map
-  {};
-  template<typename Tkey>
-  class unordered_map<Tkey, std::deque<Tkey>, std::deque<std::pair<Tkey, std::deque<Tkey> > > >
-  {
-  public:
-    unordered_map(void) : mMap() {}
-    std::deque<Tkey>& operator[](Tkey key)
-    {
-      for (typename std::deque<std::pair<Tkey, std::deque<Tkey> > >::iterator it = mMap.begin(); it != mMap.end(); ++it)
-      {
-        if (it->first == key)
-        {
-          return it->second;
-        }
-      }
-      mMap.push_back(std::pair<Tkey, std::deque<Tkey> >(key, std::deque<Tkey>()));
-      return mMap.back().second;
-    }
-  private:
-    std::deque<std::pair<Tkey, std::deque<Tkey> > > mMap;
-  };
-  template<typename Tkey>
-  class unordered_map<Tkey, std::vector<Tkey>, std::vector<std::pair<Tkey, std::vector<Tkey> > > >
-  {
-  public:
-    unordered_map(void) : mMap() {}
-    std::vector<Tkey>& operator[](Tkey key)
-    {
-      for (typename std::vector<std::pair<Tkey, std::vector<Tkey> > >::iterator it = mMap.begin(); it != mMap.end(); ++it)
-      {
-        if (it->first == key)
-        {
-          return it->second;
-        }
-      }
-      mMap.push_back(std::pair<Tkey, std::vector<Tkey> >(key, std::vector<Tkey>()));
-      return mMap.back().second;
-    }
-  private:
-    std::vector<std::pair<Tkey, std::vector<Tkey> > > mMap;
-  };
 private:
   PmergeMe& operator=(const PmergeMe& copy);
 };
